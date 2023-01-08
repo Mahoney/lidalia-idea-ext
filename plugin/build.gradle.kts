08ec575324bcf35bb66a4cfcb9f184ae467d409a
Robert Elliot <rob@lidalia.org.uk>
2023-01-08 14:21:12 +0000

    Initial commit

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,68 @@
+import java.net.URI
+
+@Suppress("DSL_SCOPE_VIOLATION") // Remove once KTIJ-19369 is fixed
+plugins {
+  `java-gradle-plugin`
+  `kotlin-dsl`
+  alias(libs.plugins.kotlin)
+  `maven-publish`
+  alias(libs.plugins.downloaddeps)
+}
+
+repositories {
+  mavenCentral()
+  gradlePluginPortal()
+}
+
+dependencies {
+  api(libs.gradle.plugin.idea.ext)
+  testImplementation(libs.bundles.kotest)
+  testImplementation(libs.gradle.plugin.kotlin.jvm)
+}
+
+gradlePlugin {
+  // Define the plugin
+  @Suppress("UNUSED_VARIABLE")
+  val ideaExt by plugins.creating {
+    id = "uk.org.lidalia.ideaext"
+    version = "0.1.0"
+    implementationClass = "uk.org.lidalia.gradle.plugin.ideaext.LidaliaIdeaExtPlugin"
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "lidalia-public"
+      url = URI("s3://lidalia-maven-public-repo/releases/")
+      credentials(AwsCredentials::class.java) {
+        accessKey = System.getenv("AWS_ACCESS_KEY_ID")
+        secretKey = System.getenv("AWS_SECRET_ACCESS_KEY")
+        sessionToken = System.getenv("AWS_SESSION_TOKEN")
+      }
+    }
+  }
+}
+
+// Add a source set for the functional test suite
+val functionalTestSourceSet = sourceSets.create("functionalTest") {
+}
+
+configurations["functionalTestImplementation"].extendsFrom(configurations["testImplementation"])
+
+// Add a task to run the functional tests
+val functionalTest by tasks.registering(Test::class) {
+  testClassesDirs = functionalTestSourceSet.output.classesDirs
+  classpath = functionalTestSourceSet.runtimeClasspath
+}
+
+gradlePlugin.testSourceSets(functionalTestSourceSet)
+
+tasks.named<Task>("check") {
+  // Run the functional tests as part of `check`
+  dependsOn(functionalTest)
+}
+
+tasks.withType<Test>().configureEach {
+  useJUnitPlatform()
+}
---
+++ plugin/src/functionalTest/kotlin/LidaliaIdeaExtPluginFunctionalTest.kt
@@ -0,0 +1,50 @@
+package uk.org.lidalia.gradle.plugin.ideaext
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.engine.spec.tempdir
+import io.kotest.matchers.collections.shouldBeEmpty
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome.FAILED
+import org.intellij.lang.annotations.Language
+
+/**
+ * A simple functional test for the 'uk.org.lidalia.ideaext' plugin.
+ */
+class LidaliaIdeaExtPluginFunctionalTest : StringSpec({
+
+  val tempFolder = tempdir()
+
+  fun getProjectDir() = tempFolder
+  fun getBuildFile() = getProjectDir().resolve("build.gradle.kts")
+  fun getSettingsFile() = getProjectDir().resolve("settings.gradle.kts")
+
+  "can apply plugin" {
+    // Setup the test build
+    getSettingsFile().writeText("")
+    @Language("kotlin")
+    val buildFile = """
+      plugins {
+//        kotlin("jvm") version "1.7.22"
+        id("uk.org.lidalia.ideaext")
+      }
+      
+      idea {
+        setPackagePrefix("com.example.foo")
+      }
+    """.trimIndent()
+    getBuildFile().writeText(
+      buildFile
+    )
+
+    // Run the build
+    val result = GradleRunner.create()
+      .forwardOutput()
+      .withPluginClasspath()
+      .withArguments("--info", "build")
+      .withProjectDir(getProjectDir())
+      .build()
+
+    val failedTasks = result.tasks.filter { it.outcome == FAILED }
+    failedTasks.shouldBeEmpty()
+  }
+})
